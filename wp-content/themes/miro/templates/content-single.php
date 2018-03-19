1cc7a3a6ce46dd5664fe45a2d7136f3c3593c8fd
Michael Church <michael@fishinglounge.com>
2018-03-19 19:25:06 -0400

    progress dev

--- wp-content/themes/miro/templates/content-single.php
+++ wp-content/themes/miro/templates/content-single.php
@@ -1,15 +1,21 @@
 <?php 
-  $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'square' );
+  $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'sixteen-nine' );
   $url = $thumb['0'];
   $img_id = get_post_thumbnail_id(get_the_ID());
   $alt_text = get_post_meta($img_id , '_wp_attachment_image_alt', true);
+
+	$id = get_post_thumbnail_id();
+	$src = wp_get_attachment_image_src( $id, 'full' );
+	$srcset = wp_get_attachment_image_srcset( $id, 'full' );
+	$sizes = wp_get_attachment_image_sizes( $id, 'full' );
+	$alt = get_post_meta( $id, '_wp_attachment_image_alt', true);
 ?>
 <?php while (have_posts()) : the_post(); ?>
 <section class="section-padding">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-10 m-auto">
-				<h1 class="display-4 mb-3"><?php the_title(); ?></h1>
+				<h1 class="display-3 mb-3"><?php the_title(); ?></h1>
 				<div class="d-flex flex-row align-items-center mb-5">
 					<div>
 						<?php  global $post;
@@ -25,6 +31,22 @@
 				</div>
 				<div class="mb-5"><?php echo do_shortcode('[addthis]');?></div>
 				
+			</div>			
+		</div>
+	</div>
+</section>
+<section class="sixteen-nine mdc-bg-green-500">
+<img src="<?php echo $url;?>" >
+</section>
+
+
+
+<section class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-10 col-lg-8 m-auto">
+				<p class="mb-5"><img src="<?php echo esc_attr( $id );?>" srcset="<?php echo esc_attr( $srcset ); ?>" sizes="<?php echo esc_attr( $sizes );?>" alt="<?php echo esc_attr( $alt );?>" class="img-fluid" /></p>
+				<?php the_content(); ?>
 			</div>			
 		</div>
 	</div>
