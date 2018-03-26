5cbee3dd7d866d19b9f3695246fcedb02618cad3
Michael Church <michael@fishinglounge.com>
2018-03-26 18:03:39 -0400

    progress dev

--- wp-content/themes/miro/lib/extras.php
+++ wp-content/themes/miro/lib/extras.php
@@ -256,10 +256,11 @@ function WPTime_add_custom_class_to_all_images($content){
 }
 add_filter('the_content', __NAMESPACE__ . '\\WPTime_add_custom_class_to_all_images');
 
-//Add Custom Image Sices to Media Uploader
+//Add Custom Image Sizes to Media Uploader
 function my_custom_image_sizes( $sizes ) {
     return array_merge( $sizes, array(
         'sixteen-nine' => __( 'Sixteen Nine' ),
+        'sixteen-nine-large' => __( 'Sixteen Nine Large' ),
     ) );
 }
 add_filter( 'image_size_names_choose', __NAMESPACE__ . '\\my_custom_image_sizes' );
--- wp-content/themes/miro/lib/setup.php
+++ wp-content/themes/miro/lib/setup.php
@@ -108,7 +108,7 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), '', '4.0.31-beta');
+  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), '', '4.0.32-beta');
   wp_register_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,900', false, null);
 	wp_enqueue_style('google-fonts');
   wp_deregister_script('jquery');
